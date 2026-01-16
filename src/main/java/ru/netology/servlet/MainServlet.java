4eb7958c2e669f349b96845d0148c655cba003d1
SamMih92 <mikhaylov_s@netbro.media>
2026-01-16 13:55:39 +0300

    Refactoring MainServlet

--- src/main/java/ru/netology/servlet/MainServlet.java
+++ src/main/java/ru/netology/servlet/MainServlet.java
@@ -7,9 +7,16 @@ import ru.netology.service.PostService;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.regex.Pattern;
 
 public class MainServlet extends HttpServlet {
     private PostController controller;
+
+    // Константы для путей и статусов
+    private static final String API_POSTS_PATH = "/api/posts";
+    private static final Pattern POST_ID_PATTERN = Pattern.compile(API_POSTS_PATH + "/(\\d+)");
+    private static final int STATUS_NOT_FOUND = HttpServletResponse.SC_NOT_FOUND;
+    private static final int STATUS_INTERNAL_ERROR = HttpServletResponse.SC_INTERNAL_SERVER_ERROR;
 
     @Override
     public void init() {
@@ -20,35 +27,46 @@ public class MainServlet extends HttpServlet {
 
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) {
-        // если деплоились в root context, то достаточно этого
         try {
-            final var path = req.getRequestURI();
-            final var method = req.getMethod();
-            // primitive routing
-            if (method.equals("GET") && path.equals("/api/posts")) {
-                controller.all(resp);
+            final String path = req.getRequestURI();
+            final String method = req.getMethod();
+
+            // Роутинг по методам и путям
+            if ("GET".equals(method)) {
+                handleGet(path, resp);
                 return;
             }
-            if (method.equals("GET") && path.matches("/api/posts/\\d+")) {
-                // easy way
-                final var id = Long.parseLong(path.substring(path.lastIndexOf("/")));
-                controller.getById(id, resp);
-                return;
-            }
-            if (method.equals("POST") && path.equals("/api/posts")) {
+            if ("POST".equals(method) && API_POSTS_PATH.equals(path)) {
                 controller.save(req.getReader(), resp);
                 return;
             }
-            if (method.equals("DELETE") && path.matches("/api/posts/\\d+")) {
-                // easy way
-                final var id = Long.parseLong(path.substring(path.lastIndexOf("/")));
+            if ("DELETE".equals(method) && POST_ID_PATTERN.matcher(path).matches()) {
+                final long id = extractIdFromPath(path);
                 controller.removeById(id, resp);
                 return;
             }
-            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+
+            resp.setStatus(STATUS_NOT_FOUND);
         } catch (Exception e) {
             e.printStackTrace();
-            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            resp.setStatus(STATUS_INTERNAL_ERROR);
         }
+    }
+
+    // Приватный метод для обработки GET
+    private void handleGet(String path, HttpServletResponse resp) throws Exception {
+        if (API_POSTS_PATH.equals(path)) {
+            controller.all(resp);
+        } else if (POST_ID_PATTERN.matcher(path).matches()) {
+            final long id = extractIdFromPath(path);
+            controller.getById(id, resp);
+        } else {
+            resp.setStatus(STATUS_NOT_FOUND);
+        }
+    }
+
+    // Извлечение ID из пути (убирает дублирование)
+    private long extractIdFromPath(String path) {
+        return Long.parseLong(path.substring(path.lastIndexOf("/") + 1));
     }
 }
